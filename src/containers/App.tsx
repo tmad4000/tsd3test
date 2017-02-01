91097d4ceb9c9e1eb6f7a82c3ad56a7d320aff8d
Jacob Cole <tmad4000@gmail.com>
2017-01-31 16:49:12 -0800

    working, no keys

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -3,94 +3,52 @@ import * as React from "react";
 import * as d3 from "d3";
 import * as _ from "lodash";
 // import * as Infinite from "react-infinite";
-var Infinite = require("react-infinite");
+
+import D3List from "./D3List"
+
+// var Infinite = require("react-infinite");
 
 
-// export interface ListViewState {
-
-
-export interface AppProps extends React.Props<App> {
-
+export interface AppState {
+    numbers?: number[]
 }
 
 
-export default class App extends React.Component<AppProps, undefined> {
-    mountPoint: HTMLElement
-    numbers: number[] = _.times(3000, () => 3);
+export interface AppProps extends React.Props<App> {
+}
 
+
+export default class App extends React.Component<AppProps, AppState> {
 
     constructor(props) {
 
         super(props);
+        this.state = {
+            numbers: _.times(3000, () => 3)
+        }
 
         //    this.state = initState
 
     }
 
-    componentDidMount() {
-
-        const d3MountPoint = d3.select(this.mountPoint)
-
-        let update = () => {
-            let sel = d3MountPoint.select("#list").selectAll("li")
-
-
-            let datSel = sel.data(this.numbers)
-                .text((d) => d).style("border", "1px solid black")
-
-            let enterSel = datSel.enter()
-            enterSel.append("li")
-                .text((d) => d).style("border", "1px solid black")
-
-            let exitSel = sel.exit()
-            exitSel.remove()
-
-            d3MountPoint.select("#list").selectAll("li").style("border", "1px solid black")
-
-        }
-
-        d3MountPoint.select("#append").on("click", () => {
-            this.numbers.unshift(8)
-            update()
-        }
-        )
-
-        update()
-
-        //     .data([4, 8, 15, 16, 23, 42])
-        // // .text(function (d) { return d; });
-
-        // p.data([1])
-
-        // // Enter…
-        // p.enter().append("p")
-        //     .text(function (d) { return d; }).style("border:1px solid black");
-        // // Exit…
-        // p.exit().remove();
-
-
-    }
-
-
 
     render() {
 
-                // <ul id="list">
-                //     <Infinite containerHeight={400} elementHeight={20}>
-                //         <li height="20">uu</li>
-                //     </Infinite>
-                // </ul>
+        // <ul id="list">
+        //     <Infinite containerHeight={400} elementHeight={20}>
+        //         <li height="20">uu</li>
+        //     </Infinite>
+        // </ul>
 
 
         return (<div>
-            Titleasd fa
-            <div ref={mountPoint => this.mountPoint = mountPoint} >
-                <button id="append">Append</button>
-                <ul id="list">
-                        <li height="20">uu</li>
-                </ul>
-
-            </div>
+            Titleasd faadsfa
+            <D3List
+                data={this.state.numbers}
+                addNum={() => this.setState((prevState) => {
+                    return { numbers: [888].concat(prevState.numbers) }
+                }) }
+                />
 
 
         </div>)
---
+++ src/containers/D3List.tsx
@@ -0,0 +1,118 @@
+import * as React from "react";
+
+import * as d3 from "d3";
+import * as _ from "lodash";
+// import * as Infinite from "react-infinite";
+var Infinite = require("react-infinite");
+
+
+// export interface ListViewState {
+
+
+export interface D3ListProps extends React.Props<D3List> {
+    data: number[]
+    addNum: () => void
+}
+
+
+export default class D3List extends React.Component<D3ListProps, undefined> {
+    mountPoint: HTMLElement
+
+
+    constructor(props) {
+
+        super(props);
+
+        //    this.state = initState
+
+    }
+
+    componentDidMount() {
+
+        const d3MountPoint = d3.select(this.mountPoint)
+
+        d3MountPoint.select("#append").on("click", () => {
+            this.props.addNum()
+        })
+
+        this.updateData(this.props)
+
+
+
+
+
+
+        // update()
+
+        //     .data([4, 8, 15, 16, 23, 42])
+        // // .text(function (d) { return d; });
+
+        // p.data([1])
+
+        // // Enter…
+        // p.enter().append("p")
+        //     .text(function (d) { return d; }).style("border:1px solid black");
+        // // Exit…
+        // p.exit().remove();
+
+
+    }
+
+
+    componentWillReceiveProps(nextProps: D3ListProps) {
+        this.updateData(nextProps)
+    }
+
+
+    updateData = (props: D3ListProps) => {
+        const d3MountPoint = d3.select(this.mountPoint)
+
+
+        let sel = d3MountPoint.select("#list").selectAll("li")
+
+
+        let datSel = sel.data(props.data)
+            .text((d) => d)
+            // .style("border", "1px solid black")
+
+        let enterSel = datSel.enter()
+        enterSel.append("li")
+            .text((d) => d)
+        // .style("border", "1px solid red")
+
+        let exitSel = datSel.exit()
+        exitSel.remove()
+
+
+    }
+
+
+
+
+    render() {
+
+        // <ul id="list">
+        //     <Infinite containerHeight={400} elementHeight={20}>
+        //         <li height="20">uu</li>
+        //     </Infinite>
+        // </ul>
+
+
+        return (<div >
+            Titleasd faadsfa
+            < div ref= { mountPoint => this.mountPoint = mountPoint } >
+                <button id="append">Append</button>
+                <ul id="list">
+                    <li height="20">uu</li>
+                    <li height="20">uu</li>
+                    <li height="20">uu</li>
+                </ul>
+
+            </div >
+
+
+        </div >)
+
+
+    }
+}
